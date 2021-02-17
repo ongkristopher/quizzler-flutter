debce0739b170accf8218cab071f9d9056a7022a
Kristopher Ong <ongkristopher@gmail.com>
2021-02-18 00:50:53 +0800

    Added functionality to show question by setState() Interim: app crashing due to questions' array exceeded

--- lib/main.dart
+++ lib/main.dart
@@ -26,16 +26,14 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
 
-  List<Widget> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green
-  ),
-    Icon(
-        Icons.close,
-        color: Colors.red
-    )
+  List<Widget> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -48,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -75,12 +73,11 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
                 setState(() {
-                  scoreKeeper.add(
-                      Icon(Icons.check, color: Colors.green)
-                  );
+                  questionNumber++;
                 });
+
+                //The user picked true
               },
             ),
           ),
@@ -101,6 +98,10 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(() {
+                  questionNumber++;
+                });
+
                 //The user picked false.
               },
             ),
