655fbcdf30dcc18f1bd19d0f96e296a53f2d3270
Kristopher Ong <ongkristopher@gmail.com>
2021-02-21 01:30:45 +0800

    Abstracted the code on questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import "package:quizzler/quiz_brain.dart";
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -27,11 +30,8 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
 
   List<Widget> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
+
+
 
   int questionNumber = 0;
   @override
@@ -46,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,6 +73,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
+
+                if(correctAnswer == true){
+                  print("correct");
+                } else {
+                  print("wrong");
+                }
+
                 setState(() {
                   questionNumber++;
                 });
@@ -98,6 +106,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
+
+                if(correctAnswer == false){
+                  print("correct");
+                } else {
+                  print("wrong");
+                }
                 setState(() {
                   questionNumber++;
                 });
